dd0bbd961815e53d1492ed0f2f57bba3133869f4
Matthew Stevens <39695231+baabcantcode@users.noreply.github.com>
2023-01-15 22:17:57 -0800

    cleaned up, working

---
+++ csv_transform/README.md
@@ -0,0 +1,6 @@
+# cli_transform
+
+## running this from CLI for testing / actual use
+``` sh
+cargo run -- -f testin.csv "select * from table1 where big = 'feels'" testout.csv
+```
--- csv_transform/src/main.rs
+++ csv_transform/src/main.rs
@@ -1,10 +1,10 @@
 use anyhow::{bail, Result};
 use clap::Parser;
-use csv::{Reader, StringRecord};
-use sqlx::{decode, sqlite, Row, SqlitePool};
-use sqlx::{Sqlite, Value, ValueRef};
-use std::io::BufRead;
-use tokio::{io::AsyncWriteExt, main};
+use csv::Reader;
+use sqlx::sqlite::SqliteRow;
+use sqlx::Sqlite;
+use sqlx::{Row, SqlitePool};
+use std::io::prelude::*;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -16,7 +16,9 @@ Use this cli to run sql queries to transform an in file into an outfile.
 Runs on sqlite, you can refer to standard sqlite docs for query rules.
 example usage with a csv at ./thiscsv.csv
 
-csv_transform -f thiscsv.csv out.csv "select * from file where id = '235'"
+csv_transform -f thiscsv.csv "select * from table1 where id = '235'" out.csv
+
+the output argument is optional, if skipped will print to stdout
 
 the initial table created from the csv will be named 'table1' and additional tables
 will be named in order that they were given to this cli -> table2, table3 etc
@@ -28,20 +30,19 @@ struct Cli {
     /// the intake csv to transform
     #[arg(short, long)]
     fileinput: Vec<String>,
-    /// the file the final queries result will be outputted to
-    outcsv: String,
     /// the sqlite query used to transform the data
     sql: String,
-
-    /// default: false. If unnamed, the columns will be named as col1, col2, etc. and the first
-    /// row in the csv will be considered as the first row of data
-    #[arg(short, long)]
-    unnamed: Option<bool>,
+    /// the file the final queries result will be outputted to
+    outcsv: Option<String>,
 }
 
-async fn insert_data_helper(ins_starter: String, values: &Vec<String>, sql: &Vec<String>, pool: &sqlx::Pool<Sqlite>,) -> Result<()> {
-    let sql_string = format!("{} {}", ins_starter, sql.join(","));
-    let mut qb = sqlx::query(&sql_string);
+async fn insert_data_helper(
+    sql_string: &str,
+    values: &[String],
+    pool: &sqlx::Pool<Sqlite>,
+) -> Result<()> {
+    // println!("insert: {} values: {}", sql_string, values.join(" ->> "));
+    let mut qb = sqlx::query(sql_string);
     for value in values.iter() {
         qb = qb.bind(value);
     }
@@ -55,23 +56,20 @@ async fn insert_data(
     tablename: String,
     headers: Vec<String>,
 ) -> Result<()> {
-    let mut sql = Vec::<String>::new();
-    sql.push("INSERT INTO ".to_string());
-    sql.push(tablename);
-    sql.push(" (".to_string());
+    let mut sql = vec!["INSERT INTO ".to_string(), tablename, "(".to_string()];
     sql.push(headers.join(","));
-    sql.push(") VALUES ".to_string());
+    sql.push(")VALUES".to_string());
     let insert_starter = sql.join("");
     sql.clear();
 
-    for i in 0..headers.len() {
+    for _ in 0..headers.len() {
         sql.push("?".to_string());
     }
     let placeholders = format!("({})", sql.join(","));
     sql.clear();
     let mut values = Vec::<String>::new();
 
-    static INS_MAX: i64 = 2;
+    static INS_MAX: i64 = 300;
     let mut i = 0;
     for line in reader.records() {
         i += 1;
@@ -79,7 +77,8 @@ async fn insert_data(
         let mut bindable: Vec<String> = line?.deserialize(None)?;
         values.append(&mut bindable);
         if i >= INS_MAX {
-            insert_data_helper(insert_starter.clone(), &values, &sql, &pool).await?;
+            let sql_string = format!("{}{}", insert_starter, sql.join(","));
+            insert_data_helper(&sql_string, &values, pool).await?;
             sql.clear();
             values.clear();
         }
@@ -87,7 +86,8 @@ async fn insert_data(
     if sql.is_empty() {
         return Ok(());
     }
-    insert_data_helper(insert_starter.clone(), &values, &sql, &pool).await?;
+    let sql_string = format!("{}{}", insert_starter, sql.join(","));
+    insert_data_helper(&sql_string, &values, pool).await?;
     Ok(())
 }
 
@@ -95,56 +95,75 @@ fn read_csv(cli: Vec<String>) -> Result<(String, Vec<String>, Reader<std::fs::Fi
     let input = std::fs::File::open(cli.into_iter().next().unwrap())?;
     let mut reader = Reader::from_reader(input);
     let header: Vec<String> = reader.headers().unwrap().deserialize(None)?;
-    if header.len() < 1 {
+    if header.is_empty() {
         bail!("empty csv given");
     }
-    let mut sql = Vec::<String>::new();
-    sql.push(
+    let mut sql = vec![
         r#"
 create table table1 
 (         
         "#
         .to_string(),
-    );
-    sql.push(header.join(
-        r#" STRING NOT NULL DEFAULT '', 
+        header.join(
+            r#" STRING NOT NULL DEFAULT '', 
         "#,
-    ));
+        ),
+    ];
     sql.push(" STRING NOT NULL DEFAULT '' ".to_string());
     sql.push(")".to_string());
     Ok((sql.join(" "), header, reader))
-    // let mut records = Vec::<StringRecord>::new();
-
-    // for line in reader.records() {
-    //     records.push(line?);
-    // }
-    // if records.len() < 1 {
-    //     bail!("only headers csv given");
-    // }
-    // let mut record_iter = records.iter();
-    // let _record: Vec<String> = record_iter.next().unwrap().deserialize(None)?;
-
-    // Ok(sql_string)
 }
 
-#[main]
+fn read_queried_data(run_q: &[SqliteRow]) -> Result<String> {
+    let mut results = Vec::<String>::new();
+    let mut col_count = 0;
+    for row in run_q {
+        col_count = row.len();
+        let mut intermediary = Vec::<String>::new();
+        for i in 0..col_count {
+            let v8: Vec<u8> = row.try_get_unchecked(i)?;
+            let mut unescaped_str: String = std::str::from_utf8(&v8)?.to_string();
+            if unescaped_str.find(',').is_some() {
+                unescaped_str = format!("\"{}\"", unescaped_str);
+            }
+            intermediary.push(unescaped_str);
+        }
+        results.push(intermediary.join(","));
+        results.push("\n".to_string());
+    }
+
+    Ok(format!(
+        "{}\n{}",
+        (1..col_count + 1)
+            .map(|x| { format!("col{}", x) })
+            .collect::<Vec<String>>()
+            .join(","),
+        results.join("")
+    ))
+}
+
+#[tokio::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
     let pool = SqlitePool::connect("sqlite::memory:").await?;
     let (sql, headers, reader) = read_csv(cli.fileinput)?;
     sqlx::query(&sql).execute(&pool).await?;
     insert_data(&pool, reader, "table1".to_string(), headers).await?;
-    let mut run_q = sqlx::query("select * from table1").fetch_all(&pool).await?;
-    if run_q.len() < 1 {
+    let run_q = sqlx::query(&cli.sql).fetch_all(&pool).await?;
+    if run_q.is_empty() {
         bail!("no records returned for query:\n{}", cli.sql);
     }
-    // let col1: String = run_q
-    //     .iter_mut()
-    //     .next()
-    //     .unwrap()
-    //     .try_get_raw(1)?
-    //     .to_owned()
-    //     .try_decode()?;
-    // println!("{}", col1);
+    let to_write = read_queried_data(&run_q)?;
+
+    match cli.outcsv {
+        None => {
+            println!("\nresults:\n\n{}", to_write);
+        }
+        Some(outcsv) => {
+            let mut writer = std::fs::File::create(outcsv)?;
+            writer.write_all(to_write.as_bytes())?;
+        }
+    }
+
     Ok(())
 }
